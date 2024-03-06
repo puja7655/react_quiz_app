f09a096e9d7a42b556b1437f2f0e80d7af156133
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-03-06 23:32:48 +0100

    added fallback screen once number of questions are finished

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react"
 import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {
     const [usersAnswers, setUsersAnswers] = useState([])
     const activeQuestionIndex = usersAnswers.length
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     function handleSelectAnswer(selectedAnswer) {
-        if (QUESTIONS.length) {
-            setUsersAnswers((prevUserAnswer) => {
-                return [...prevUserAnswer, selectedAnswer]
-            })
-        }
-
+        setUsersAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer]
+        })
         console.log("answers", usersAnswers)
     }
+
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Quiz is complete" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random - 0.5) //if sort function returns negative value it swaps the input.here with Math.random i will get numbers between 0 to 1 and substracting .5 will give me mostly negative value
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) =>
+                    {shuffledAnswers.map((answer) =>
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
