ed3ec2ecae1575590fd73d218086604f752ff3e6
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-03-07 15:45:10 +0100

    answer status added with css class

---
+++ src/components/Answers.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+
+export default function Answers({ answers, selectedAnswer, answerState, onSelect }) {
+
+    const shuffledAnswers = useRef()
+
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers]
+        shuffledAnswers.current.sort(() => Math.random - 0.5) //if sort function returns negative value it swaps the input.here with Math.random i will get numbers between 0 to 1 and substracting .5 will give me mostly negative value
+
+    }
+    return <ul id="answers">
+        {shuffledAnswers.current.map((answer) => {
+            let isSelected = selectedAnswer === answer;
+            let cssClass = ''
+
+            if (answerState === 'answered' && isSelected) {
+                cssClass = 'selected'
+            }
+            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                cssClass = answerState
+            }
+            return <li key={answer} className="answer">
+                <button
+                    onClick={() => onSelect(answer)}
+                    className={cssClass}>{answer}</button>
+            </li>
+        }
+        )}
+    </ul>
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,18 +2,39 @@ import { useCallback, useState } from "react"
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png'
 import QuestionTimer from '../components/QuestioonTimer.jsx'
+import Answers from "./Answers.jsx"
 
 export default function Quiz() {
     const [usersAnswers, setUsersAnswers] = useState([])
-    const activeQuestionIndex = usersAnswers.length
+    const [answerState, setAnswerState] = useState('')
+
+    // This is done as once the user selects an answer activeQuestionIndex points to the next question but to check
+    //if the selected answer was right or wrong
+    const activeQuestionIndex =
+        answerState === '' ? usersAnswers.length : usersAnswers.length - 1;
+
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        setAnswerState('answered')
         setUsersAnswers((prevUserAnswer) => {
             return [...prevUserAnswer, selectedAnswer]
         })
-        console.log("answers", usersAnswers)
-    }, [])
+
+        setTimeout(() => {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct')
+            } else {
+                setAnswerState('wrong')
+            }
+
+            //Reseting the answer state after answer state has been identified
+            setTimeout(() => {
+                setAnswerState('')
+            }, 2000)
+        }, 1000)
+
+    }, [activeQuestionIndex])
 
     const handleSkipQuestion = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
@@ -26,8 +47,6 @@ export default function Quiz() {
         )
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffledAnswers.sort(() => Math.random - 0.5) //if sort function returns negative value it swaps the input.here with Math.random i will get numbers between 0 to 1 and substracting .5 will give me mostly negative value
 
     //it is very important to add key . Here in QuestionTimer if we do not add key ,and when question changes since there is no change in QuestionTimer
     //it will not be re-rendered . Hence progres bar would show nothing.With key in place there is some change in there and QuestionTimer would re-render
@@ -40,13 +59,12 @@ export default function Quiz() {
             />
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map((answer) =>
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    )}
-                </ul>
+                <Answers 
+                key={activeQuestionIndex}
+                answers={QUESTIONS[activeQuestionIndex].answers} 
+                selectedAnswer={usersAnswers[usersAnswers.length - 1]} 
+                answerState={answerState}
+                onSelect={handleSelectAnswer}/>
             </div>
         </div>
 
